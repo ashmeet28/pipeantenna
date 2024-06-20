3267a5849022cab2201d1591f7bd7f761060d3d8
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2024-06-19 22:37:02 -0400

    Improved

--- pipeantenna.go
+++ pipeantenna.go
@@ -4,73 +4,38 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 )
 
 func main() {
-	http.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Disposition", "attachment; filename=\""+os.Args[1]+"\"")
-		w.Header().Set("Content-Type", "application/octet-stream")
+	var l sync.Mutex
 
+	http.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
+		l.Lock()
+		w.Header().Set("Content-Disposition",
+			"attachment; filename=\""+time.Now().Format("20060102_150405")+"\"")
+		w.Header().Set("Content-Type", "application/octet-stream")
 		io.Copy(w, os.Stdin)
-		go func() {
-			time.Sleep(5 * time.Second)
+		time.AfterFunc(3*time.Second, func() {
 			os.Exit(0)
-		}()
+		})
 	})
 
 	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		l.Lock()
 		io.Copy(os.Stdout, r.Body)
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("File uploaded successfully"))
-		go func() {
-			time.Sleep(5 * time.Second)
+		time.AfterFunc(3*time.Second, func() {
 			os.Exit(0)
-		}()
+		})
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
-		html := `
-			<!DOCTYPE html>
-			<html lang="en">
-			<head>
-				<meta charset="UTF-8">
-				<title>File Upload</title>
-				<script>
-					function uploadFile(fileInput) {
-						const file = fileInput.files[0];
-						if (!file) {
-							alert('Please select a file');
-							return;
-						}
-
-						alert("Starting file upload")
-
-						const xhr = new XMLHttpRequest();
-						xhr.open('POST', '/upload', true);
-						xhr.setRequestHeader('Content-Type', file.type);
-						xhr.onload = function () {
-							if (xhr.status === 200) {
-								alert(xhr.responseText);
-							}
-						};
-						xhr.send(file);
-					}
-				</script>
-			</head>
-			<body>
-				<h1>File Upload</h1>
-				<input type="file" id="fileInput" />
-				<button onclick="uploadFile(document.getElementById('fileInput'))">Upload File</button>
-				<h1>File Download</h1>
-				<form action="/download" method="get">
-					<button type="submit">Download File</button>
-				</form>
-			</body>
-			</html>
-		`
-		io.WriteString(w, html)
+		d, _ := os.ReadFile("web_pages/index.html")
+		w.Write(d)
 	})
 
 	http.ListenAndServe(":8080", nil)
